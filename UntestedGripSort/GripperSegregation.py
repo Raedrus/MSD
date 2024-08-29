825b915f6fa8491cf01bdcadeae7256969e2ef58
Rae <dingyanyan483@gmail.com>
2024-08-29 17:15:15 +0800

    Update Commit

--- UntestedGripSort/GripperSegregation.py
+++ UntestedGripSort/GripperSegregation.py
@@ -1,298 +1,280 @@
+# Gripping cycle refers to:
+# Object detection --> obtain number of objects and coordinates --> gripper sorting until no items remain
 
-#Gripping cycle refers to:
-#Object detection --> obtain number of objects and coordinates --> gripper sorting until no items remain
+import CONST  # constant values only, coordinates and such
 
-import CONST #constant values only, coordinates and such
-
-import ImgWasteData as imgD #Image Data
-import V33_XYMovementControls_XY as gan #Gantry
+import ImgWasteData as imgD  # Image Data
+import V33_XYMovementControls_XY as gan  # Gantry
 import ZAxis as za
 
 import serial
 from Serial_Pi_ESP32 import esp32_comms
 
+# Variables##########################################
+gripper_posi = [0, 0]  # Current gripper coordinates in steps
 
-#Variables##########################################
-gripper_posi = [0, 0] #Current gripper coordinates in steps
+# intentional
 
-intentional
-
-#Class declarations#################################
+# Class declarations#################################
 ser = serial.Serial('/dev/ttyAMA0', 115200, timeout=1)
+
+
 ####################################################
 
 def home_XY():
-    #Home the axis at the start and at the very end
-    
-    gan.drivedrv8825(ysteps_toDesti, dirY, "Full", "Y", 0.0004, homing = True)
-    gan.drivedrv8825(ysteps_toDesti, dirY, "Full", "X", 0.0004, homing = True)
+    # Home the axis at the start and at the very end
+
+    gan.drivedrv8825(ysteps_toDesti, dirY, "Full", "Y", 0.0004, homing=True)
+    gan.drivedrv8825(ysteps_toDesti, dirY, "Full", "X", 0.0004, homing=True)
     esp32_comms(ser, "open")
 
 
-#The 4 bins (General waste, metal, electronics, batteries)
-#One's with gripping requirements: Metals (Aluminum can), electronics, batteries
-    
+# The 4 bins (General waste, metal, electronics, batteries)
+# One's with gripping requirements: Metals (Aluminum can), electronics, batteries
 
-    
 
-def ToBinA(waste_sz):
-    #With assumption that the item is already picked up
-    #Take the item and drop them at Bin A
-    
-    #waste_sz in terms pixels detected from the camera, in type_posi array
-    #For now, based on box area (Not foolproof for standing items)
-    
-    xDir = 0 #Placeholder values
-    xloc = 30 #Placeholder value
-    which_encoder = 1 #Placeholder value
-    
-    
-    #The bins that drop gripping items have encoders on them, only a matter of which side
-    #which_encoder is an int of 1 or 0 to control direction (which encoder to move to)
-    #After moving to that location, xloc moves it above the right bin
-    #xDir has to be determined from checking the right way to turn the motor
-    
-    #Move the gripper set towards to x limit switch,
-    #this gives it accuracy later on when moviong to the bin
-    gan.drivedrv8825(ysteps_toDesti, dirY, "Full", "X", 0.0004, homing = True)
+def ToCoordZero():
+    # The gripper goes to 0 coordinate of the camera frame
+
+    # ASSUMING THIS ENCODER IS THE HOME POSITION ENCODER
     drivedrv8825(ysteps_toDesti,
                  dirY,
                  "Full",
                  "Y",
                  0.0004,
-                 tobin = 1) #Move the Y-axis towards one encoder
-    
-    #Move X towards the needed bin
+                 tobin=1)  # Move the Y-axis towards one encoder
+
+    gan.drivedrv8825(ysteps_toDesti, dirY, "Full", "X", 0.0004, homing=True)
+
+    gripper_posi[0] = 0
+    gripper_posi[1] = 0
+
+
+def ToBinA(waste_sz):
+    # With assumption that the item is already picked up
+    # Take the item and drop them at Bin A
+
+    # waste_sz in terms pixels detected from the camera, in type_posi array
+    # For now, based on box area (Not foolproof for standing items)
+
+    xDir = 0  # Placeholder values
+    xloc = 30  # Placeholder value
+    which_encoder = 1  # Placeholder value
+
+    # The bins that drop gripping items have encoders on them, only a matter of which side
+    # which_encoder is an int of 1 or 0 to control direction (which encoder to move to)
+    # After moving to that location, xloc moves it above the right bin
+    # xDir has to be determined from checking the right way to turn the motor
+
+    # Move the gripper set towards to x limit switch,
+    # this gives it accuracy later on when moviong to the bin
+    gan.drivedrv8825(ysteps_toDesti, dirY, "Full", "X", 0.0004, homing=True)
+    drivedrv8825(ysteps_toDesti,
+                 dirY,
+                 "Full",
+                 "Y",
+                 0.0004,
+                 tobin=1)  # Move the Y-axis towards one encoder
+
+    # Move X towards the needed bin
     drivedrv8825(0,
                  dirY,
                  "Full",
                  "XY",
                  0.001,
-                 list_DirXY = [xDir, 1],
-                 list_XYSteps = [xloc, 0],
-                 invert_xDir = True,
-                 invert_yDir = True) #Direction inversion
-    
-    
-    #Use a specific type of grip depending on size
-    
+                 list_DirXY=[xDir, 1],
+                 list_XYSteps=[xloc, 0],
+                 invert_xDir=True,
+                 invert_yDir=True)  # Direction inversion
+
+    # Use a specific type of grip depending on size
+
     if waste_sz <= CONST.SMALL_SIZE:
         vacuum_release_drop()
     else:
         finger_release_drop()
-    
-    #Move x back to it's limit switch to recalibrate
-    gan.drivedrv8825(ysteps_toDesti, dirY, "Full", "X", 0.0004, homing = True)
-    
+
+    # Move x back to it's limit switch to recalibrate
+    gan.drivedrv8825(ysteps_toDesti, dirY, "Full", "X", 0.0004, homing=True)
+
     print("Waste to Bin A was Attempted")
 
-
-    #Return the current position of the gripper after completion of the operation
-    #x = 0, because it was homed for calibration
+    # Return the current position of the gripper after completion of the operation
+    # x = 0, because it was homed for calibration
     return [0, ENC1_POSI]
 
 
 def ToBinB(waste_sz):
-    #With assumption that the item is already picked up
-    #Take the item and drop them at Bin A
-    
-    #waste_sz in terms pixels detected from the camera, in type_posi array
-    #For now, based on box area (Not foolproof for standing items)
-    
-    xDir = 0 #Placeholder values
-    xloc = 30 #Placeholder value
-    which_encoder = 0 #Placeholder value
-    
-    
-    #The bins that drop gripping items have encoders on them, only a matter of which side
-    #which_encoder is an int of 1 or 0 to control direction (which encoder to move to)
-    #After moving to that location, xloc moves it above the right bin
-    #xDir has to be determined from checking the right way to turn the motor
-    
-    #Move the gripper set towards to x limit switch,
-    #this gives it accuracy later on when moviong to the bin
-    gan.drivedrv8825(ysteps_toDesti, dirY, "Full", "X", 0.0004, homing = True)
-    drivedrv8825(ysteps_toDesti,
-                 dirY,
-                 "Full",
-                 "Y",
-                 0.0004,
-                 tobin = 1) #Move the Y-axis towards one encoder
-    
-    #Move X towards the needed bin
+    # With assumption that the item is already picked up
+    # Take the item and drop them at Bin A
+
+    # waste_sz in terms pixels detected from the camera, in type_posi array
+    # For now, based on box area (Not foolproof for standing items)
+
+    xDir = 0  # Placeholder values
+    xloc = 30  # Placeholder value
+    which_encoder = 0  # Placeholder value
+
+    # The bins that drop gripping items have encoders on them, only a matter of which side
+    # which_encoder is an int of 1 or 0 to control direction (which encoder to move to)
+    # After moving to that location, xloc moves it above the right bin
+    # xDir has to be determined from checking the right way to turn the motor
+
+    # Move the gripper set towards to x limit switch,
+    # this gives it accuracy later on when moviong to the bin
+    gan.drivedrv8825(ysteps_toDesti, dirY, "Full", "X", 0.0004, homing=True)
+    gan.drivedrv8825(ysteps_toDesti, dirY, "Full", "Y", 0.0004,
+                     homing=True)  # Assuming the bin rests in this limit switch
+
+    # Move X towards the needed bin
     drivedrv8825(0,
                  dirY,
                  "Full",
                  "XY",
                  0.001,
-                 list_DirXY = [xDir, 1],
-                 list_XYSteps = [xloc, 0],
-                 invert_xDir = True,
-                 invert_yDir = True) #Direction inversion
-    
-    
-    #Use a specific type of grip depending on size
-    
+                 list_DirXY=[xDir, 1],
+                 list_XYSteps=[xloc, 0],
+                 invert_xDir=True,
+                 invert_yDir=True)  # Direction inversion
+
+    # Use a specific type of grip depending on size
+
     if waste_sz <= CONST.SMALL_SIZE:
         vacuum_release_drop()
     else:
         finger_release_drop()
-    
-    #Move x back to it's limit switch to recalibrate
-    gan.drivedrv8825(ysteps_toDesti, dirY, "Full", "X", 0.0004, homing = True)
-    
+
+    # Move x back to it's limit switch to recalibrate
+    gan.drivedrv8825(ysteps_toDesti, dirY, "Full", "X", 0.0004, homing=True)
+
     print("Waste to Bin A was Attempted")
 
-
-    #Return the current position of the gripper after completion of the operation
-    #x = 0, because it was homed for calibration
+    # Return the current position of the gripper after completion of the operation
+    # x = 0, because it was homed for calibration
     return [0, ENC2_POSI]
-
 
 
 def ToBinC(waste_sz):
-    #With assumption that the item is already picked up
-    #Take the item and drop them at Bin A
-    
-    #waste_sz in terms pixels detected from the camera, in type_posi array
-    #For now, based on box area (Not foolproof for standing items)
-    
-    xDir = 0 #Placeholder values
-    xloc = 60 #Placeholder value
-    which_encoder = 0 #Placeholder value
-    
-    
-    #The bins that drop gripping items have encoders on them, only a matter of which side
-    #which_encoder is an int of 1 or 0 to control direction (which encoder to move to)
-    #After moving to that location, xloc moves it above the right bin
-    #xDir has to be determined from checking the right way to turn the motor
-    
-    #Move the gripper set towards to x limit switch,
-    #this gives it accuracy later on when moviong to the bin
-    gan.drivedrv8825(ysteps_toDesti, dirY, "Full", "X", 0.0004, homing = True)
-    drivedrv8825(ysteps_toDesti,
-                 dirY,
-                 "Full",
-                 "Y",
-                 0.0004,
-                 tobin = 1) #Move the Y-axis towards one encoder
-    
-    #Move X towards the needed bin
+    # With assumption that the item is already picked up
+    # Take the item and drop them at Bin A
+
+    # waste_sz in terms pixels detected from the camera, in type_posi array
+    # For now, based on box area (Not foolproof for standing items)
+
+    xDir = 0  # Placeholder values
+    xloc = 60  # Placeholder value
+    which_encoder = 0  # Placeholder value
+
+    # The bins that drop gripping items have encoders on them, only a matter of which side
+    # which_encoder is an int of 1 or 0 to control direction (which encoder to move to)
+    # After moving to that location, xloc moves it above the right bin
+    # xDir has to be determined from checking the right way to turn the motor
+
+    # Move the gripper set towards to x limit switch,
+    # this gives it accuracy later on when moviong to the bin
+    gan.drivedrv8825(ysteps_toDesti, dirY, "Full", "X", 0.0004, homing=True)
+    gan.drivedrv8825(ysteps_toDesti, dirY, "Full", "Y", 0.0004,
+                     homing=True)  # Assuming the bin rests in this limit switch
+
+    # Move X towards the needed bin
     drivedrv8825(0,
                  dirY,
                  "Full",
                  "XY",
                  0.001,
-                 list_DirXY = [xDir, 1],
-                 list_XYSteps = [xloc, 0],
-                 invert_xDir = True,
-                 invert_yDir = True) #Direction inversion
-    
-    
-    #Use a specific type of grip depending on size
-    
+                 list_DirXY=[xDir, 1],
+                 list_XYSteps=[xloc, 0],
+                 invert_xDir=True,
+                 invert_yDir=True)  # Direction inversion
+
+    # Use a specific type of grip depending on size
+
     if waste_sz <= CONST.SMALL_SIZE:
         vacuum_release_drop()
     else:
         finger_release_drop()
-    
-    #Move x back to it's limit switch to recalibrate
-    gan.drivedrv8825(ysteps_toDesti, dirY, "Full", "X", 0.0004, homing = True)
-    
+
+    # Move x back to it's limit switch to recalibrate
+    gan.drivedrv8825(ysteps_toDesti, dirY, "Full", "X", 0.0004, homing=True)
+
     print("Waste to Bin A was Attempted")
 
-
-    #Return the current position of the gripper after completion of the operation
-    #x = 0, because it was homed for calibration
+    # Return the current position of the gripper after completion of the operation
+    # x = 0, because it was homed for calibration
     return [0, ENC2_POSI]
-    
 
-#DROPS
-#There are two kinds of drops, the gripper drop or vacuum drop
+
+# DROPS
+# There are two kinds of drops, the gripper drop or vacuum drop
 def finger_release_drop():
     sleep(0.2)
-    #lower the z axis
+    # lower the z axis
     full_lower_gripper()
-    
-    #open the gripper fingers
+
+    # open the gripper fingers
     esp32_comms(ser, "open")
-    
+
     sleep(0.2)
-    #Elevate the gripper back upwards
+    # Elevate the gripper back upwards
     full_ele_gripper()
-    
-    
-    
-    
+
+
 def vacuum_release_drop():
     pass
-
 
 
 ##########################################################################################
 
 
-   
-
 def main():
+    full_ele_gripper()
+
     home_XY()
-    
-     
-    
-    #Detect the image, return the required position and item type data
-    #and box area
+
+    # Detect the image, return the required position and item type data
+    # and box area
     type_posi = imgD.GetMVData(imgD.TakePicture())
 
-    while len(type_posi[0]) > 0:
-        
-        #The output below is the coordinates of the nearest waste
-        #[x, y] in terms of steps
-        xy_steps_toDesti = gan.getshortestdist(gripper_posi, type_posi, 20)
+    ToCoordZero()
 
-        #Obtaining turning directions
+    while len(type_posi[0]) > 0:
+        # The output below is the coordinates of the nearest waste
+        # [x, y] in terms of steps
+        xy_steps_toDesti = gan.getshortestdist(gripper_posi, type_posi, 20)
+        # Obtaining turning directions
         dirX = int(xy_steps_toDesti[0]) >= 0
         dirY = int(xy_steps_toDesti[1]) >= 0
         dirXY = [dirX, dirY]
 
-        #Move the gripper to the nearest waste
+        # Move the gripper to the nearest waste
         gan.drivedrv8825(0, dirY, "Full",
-                     "XY",
-                     0.001,
-                     list_DirXY = dirXY,
-                     list_XYSteps = xy_steps_toDesti,
-                     invert_xDir = True,
-                     invert_yDir = True)
-
+                         "XY",
+                         0.001,
+                         list_DirXY=dirXY,
+                         list_XYSteps=xy_steps_toDesti,
+                         invert_xDir=True,
+                         invert_yDir=True)
 
         if waste_sz <= CONST.SMALL_SIZE:
-            pass #Where the vacuum pump is supposed to turn off
+            pass  # Where the vacuum pump is supposed to turn off
             full_lower_gripper()
-            #Turn vacuum pump on
+        # Turn vacuum pump on
         else:
-            #Open the gripper fingers
+            # Open the gripper fingers
             esp32_comms(ser, "open")
-            #Parameter specifies number of steps required for partial descent
+            # Parameter specifies number of steps required for partial descent
             za.partial_lower_gripper(pd_steps)
-            #close fingers
+            # close fingers
             esp32_comms(ser, "close")
 
+            full_ele_gripper()
+            match type_posi[0]:
+                # Providing size of the waste as input parameter
+                case "B":
+                    ToBinA(type_posi[3])
+                case "E":
+                    ToBinB(type_posi[3])
+                case "M":
+                    ToBinC(type_posi[3])
 
-        full_ele_gripper()
-
-
-        match type_posi[0]:
-            #Providing size of the waste as input parameter
-            case "B":
-                 ToBinA(type_posi[3]) 
-            case "E":
-                 ToBinB(type_posi[3])
-            case "M":
-                 ToBinC(type_posi[3])
-
-        
         type_posi = imgD.GetMVData(imgD.TakePicture())
-
-
-
-
-
--- UntestedGripSort/ZAxis.py
+++ UntestedGripSort/ZAxis.py
@@ -1,141 +1,138 @@
-#The Z-axis system codes
+# The Z-axis system codes
 
 ####################
-    #WARNING: The gripper fingers should open before descending to prevent collision
+# WARNING: The gripper fingers should open before descending to prevent collision
 ####################
 
 
-#Note, there should be 2 distinctions of levels, z axis at its lowest level allows for vacuum gripping,
-#The other value of partial descent allows it to enter finger gripping range
+# Note, there should be 2 distinctions of levels, z axis at its lowest level allows for vacuum gripping,
+# The other value of partial descent allows it to enter finger gripping range
 
 from gpiozero import DigitalOutputDevice
 from gpiozero import PWMOutputDevice
 from gpiozero import Button
 from time import sleep
 
+# Predefined Values############################################
+desired_displacement_partial = 5  # The displacement from lowest position for the gripper finger grip objects in cm
+travel_length = 20  # Value in cm from top limit switch to lower limit switch
 
-#Predefined Values############################################
-desired_displacement_partial = 5 #The displacement from lowest position for the gripper finger grip objects in cm
-travel_length = 20 #Value in cm from top limit switch to lower limit switch
+pl_desc = 200  # Partial descent, The gripper stepper takes this amount of steps to displace itself down into gripping range
 
-pl_desc =200 #Partial descent, The gripper stepper takes this amount of steps to displace itself down into gripping range
+# Pin Assignments###############################################
+# Limit switches of upper and lower limit
+z_up_lim_pin = 6
+z_lowlim_pin = 5  # A chosen pin 26 that seems unoccupied on whatsapp schematic (15 Aug, SCH_Raspberry Pi_1-Version 2_2024-08-15
 
+# XY Gantry Motors Drivers
+pin_z_drv8825_en = 25
+pin_z_drv8825_step = 8
+pin_z_drv8825_dir = 7  # 1 for clockwise, 0 for counter-clockwise
 
-#Pin Assignments###############################################
-#Limit switches of upper and lower limit
-z_up_lim_pin = 25
-z_lowlim_pin = 26 #A chosen pin 26 that seems unoccupied on whatsapp schematic (15 Aug, SCH_Raspberry Pi_1-Version 2_2024-08-15
-
-#XY Gantry Motors Drivers
-pin_z_drv8825_en = 2 
-pin_z_drv8825_step = 3
-pin_z_drv8825_dir = 4 #1 for clockwise, 0 for counter-clockwise
-
-
-#Declaration of classes#########################################
-#Setup z axis stepper
-z_drv8825_relax = DigitalOutputDevice(pin_z_drv8825_en) 
-z_drv8825_step = PWMOutputDevice(pin_z_drv8825_step, frequency = 6000)
+# Declaration of classes#########################################
+# Setup z axis stepper
+z_drv8825_relax = DigitalOutputDevice(pin_z_drv8825_en)
+z_drv8825_step = PWMOutputDevice(pin_z_drv8825_step, frequency=6000)
 z_drv8825_dir = DigitalOutputDevice(pin_z_drv8825_dir)
 
-
-#Setup Limit Switches
-ZUP_lim = Button(z_up_lim_pin, pull_up = True)
-ZLOW_lim = Button(z_lowlim_pin, pull_up = True)
-
+# Setup Limit Switches
+ZUP_lim = Button(z_up_lim_pin, pull_up=False)
+ZLOW_lim = Button(z_lowlim_pin, pull_up=False)
 
 ######################################################################
-#Temporal Data
-cur_ele = 0 #The current position of the gripper's z position
-steps_axis_length = 0 #Represents the number of steps to move from top of gripper to the bottom
+# Temporal Data
+cur_ele = 0  # The current position of the gripper's z position
+steps_axis_length = 0  # Represents the number of steps to move from top of gripper to the bottom
+
+
 ######################################################################
 
 def full_ele_gripper():
-    #Restore gripper to upmost position for coordinate Accuracy and safety
-    
+    # Restore gripper to upmost position for coordinate Accuracy and safety
+
     z_drv8825_relax.off()
-    
-    #Set Directions
-    z_drv8825_dir.off() 
-    
-    while ZUP_lim.is_pressed != True:
-        z_drv8825_step.blink(background = False,  n= 20, on_time =0.001, off_time = 0.0004)
+
+    # Set Directions
+    z_drv8825_dir.on()
+
+    while ZUP_lim.is_pressed != False:
+        z_drv8825_step.blink(background=False, n=20, on_time=0.001, off_time=0.0004)
         print("Elevating Gripper to Z Home")
-        
-    cur_ele = 0 #Home position is tuned as position 0
+
+    cur_ele = 0  # Home position is tuned as position 0
     print("Z axis homed")
 
     z_drv8825_relax.on()
 
 
 def count_steps_of_axis(desired_displacement_partial):
-    #Optional [For use of finding the total number of steps that makes up the z axis]
-    #Allows an automated way count length of z-axis in terms of number of steps
-    
-    step_ct = 0 #step_count
-    #full_ele_gripper() #Get to top most position before counting
+    # Optional [For use of finding the total number of steps that makes up the z axis]
+    # Allows an automated way count length of z-axis in terms of number of steps
 
-    
+    step_ct = 0  # step_count
+    # full_ele_gripper() #Get to top most position before counting
+
     sleep(1)
-    
+
     z_drv8825_relax.off()
     z_drv8825_dir.on()
 
-    #Start descending the gripper
+    # Start descending the gripper
     while ZLOW_lim.is_pressed != True:
-        z_drv8825_step.blink(background = False,  n= 20, on_time =0.001, off_time = 0.0004)
+        z_drv8825_step.blink(background=False, n=20, on_time=0.001, off_time=0.0004)
         print("Descending Gripper All the way down [MEASURING]")
-        step_ct += 1*20
-        
+        step_ct += 1 * 20
+
     print(f"It takes {steps_axis_length} to reach the bottom of the gripper")
-    #This info MAY turn out helpful for an estimate number of turns needed for a partial descent,
-    #simply: z-axis_length/no_steps --> distance per step
-    dist_per_step_cm = travel_length/step_ct
-    req_steps_displacement = desired_displacement_partial/dist_per_step_cm #for partial descent
-    
+    # This info MAY turn out helpful for an estimate number of turns needed for a partial descent,
+    # simply: z-axis_length/no_steps --> distance per step
+    dist_per_step_cm = travel_length / step_ct
+    req_steps_displacement = desired_displacement_partial / dist_per_step_cm  # for partial descent
+
     print("No steps to fully descend: ", step_ct)
-    
+
     step_req = int((step_ct - req_steps_displacement))
     print("Step requirement for partial descent: ", step_req)
-    
+
     sleep(0.8)
-    
+
     print("Measurement complete, returning to the top")
     full_ele_gripper()
-    
+
     z_drv8825_relax.on()
-    
-    return  step_req#Return the value for partial descent in steps
-    
-    
+
+    return step_req  # Return the value for partial descent in steps
+
 
 def partial_lower_gripper(pl_desc):
-    
     z_drv8825_relax.off()
-    
-    #Adjust direction
-    z_drv8825_dir.on() 
-    
-    #Partially descend the gripper
-    z_drv8825_step.blink(background = False,  n= pl_desc, on_time =0.001, off_time = 0.0004)
-    
+
+    # Adjust direction
+    z_drv8825_dir.on()
+
+    # Partially descend the gripper
+    z_drv8825_step.blink(background=False, n=pl_desc, on_time=0.001, off_time=0.0004)
+
     print("Partial lower completed")
     z_drv8825_relax.on()
 
 
 def full_lower_gripper():
-    #Set the directions
-    z_drv8825_relax.off() 
-    
-    #Gripper goes all the way down to perform a vacuum grip
-    while ZLOW_lim.is_pressed != True:
-        z_drv8825_step.blink(background = False,  n= 20, on_time =0.001, off_time = 0.0004)
+    # Set the directions
+    z_drv8825_dir.off()
+
+    z_drv8825_relax.off()
+
+    # Gripper goes all the way down to perform a vacuum grip
+    while ZLOW_lim.is_pressed != False:
+        z_drv8825_step.blink(background=False, n=20, on_time=0.001, off_time=0.0004)
         print("Descending Gripper All the way down [BALLOON GRIPPING]")
-        
+
     z_drv8825_relax.on()
 
 
-# full_lower_gripper()
+full_lower_gripper()
+# full_ele_gripper()
 # 
 # print("z operation complete")
 # sleep(3)
