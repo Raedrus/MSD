078d7ea19c57a67bec71ea08da0905ca1fbb472e
Alex510-10 <alxw630723@1utar.my>
2024-09-12 14:16:52 +0800

    Update dustbin_v1.py

--- dustbin_v1.py
+++ dustbin_v1.py
@@ -16,7 +16,7 @@ import os  # For testing system quits
 import psutil  # For testing system quits
 
 # Function file import
-# from WasteSorting import sortingcycle
+#from WasteSorting import sortingcycle
 from Serial_Pi_ESP32 import esp32_comms
 from Serial_Pi_ESP32 import esp32_done
 import Gripper_Segregation as GS
@@ -24,16 +24,18 @@ import Gripper_Segregation as GS
 current_system_pid = os.getpid()
 ThisSystem = psutil.Process(current_system_pid)
 
-# Platform Servo
+
+#Platform Servo
 ##TESTING PLAT STABILIZER
 from gpiozero import AngularServo
-
 pin_plat_servo = 27
-plat_servo = AngularServo(pin_plat_servo, min_pulse_width=0.0005, max_pulse_width=0.0025)
+plat_servo=AngularServo(pin_plat_servo, min_pulse_width=0.0005, max_pulse_width=0.0025)
 
-# Initialize at home position
+#Initialize at home position
 plat_servo.angle = 0
-plat_servo.angle = None
+plat_servo.angle= None
+
+
 
 ################## GPIO Assignments################################################################
 # Ultrasonic Sensor
@@ -49,6 +51,8 @@ pin_Xhome = 11
 pin_Yhome = 9
 pin_Zhome = 25
 pin_binpresence = 19
+
+
 
 # Infrared Sensor
 pin_Platorigin = 22
@@ -78,14 +82,15 @@ ult_sensor = DistanceSensor(echo=pin_ult_echo, trigger=pin_ult_trigger)
 start_button = Button(pin_start, pull_up=True)
 Estop_button = Button(pin_Estop, pull_up=True)
 
-binpresence = Button(pin_binpresence, pull_up=False)
+binpresence = Button(pin_binpresence, pull_up = False)
 
 # Limit switch and IR sensor Assignment
 
 
 # initiate arrays
 
-# initiate interrupt for E-stop
+
+
 
 # initiate serial handshake with ESP32 (TX,RX)
 ser = serial.Serial('/dev/ttyAMA0', 115200, timeout=1)
@@ -96,6 +101,55 @@ print("Serial OK")
 # initiate webcam
 webcamera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
+###############################
+def shut_system():
+    print("Emergency stop has been triggered, attempting system shutdown...")
+    
+    #Change LED indications
+    
+    
+    
+    sys.exit()
+
+# initiate interrupt for E-stop
+
+Estop_button.when_pressed = shut_system
+################################################
+
+def LED_indicators(GLED, RLED, GBlink = False, RBlink = False):
+    #Green LED controls = GLED, int 1 or 0
+    #Red LED controls = RLED, int 1 or 0
+    
+    if GLED == 1:
+        esp32_comms(ser, "GLED_ON")
+    elif GLED == 0:
+        esp32_comms(ser, "GLED_OFF")
+    if RLED  == 1:
+        esp32_comms(ser, "RLED_ON")
+    elif RLED == 0:
+        esp32_comms(ser, "RLED_OFF")
+    
+    
+    if GBlink == True:
+        #Blink the LED
+        
+        for _ in range(3):
+            esp32_comms(ser, "GLED_ON")
+            sleep(0.2)
+            esp32_comms(ser, "GLED_OFF")
+        
+        #Restore the LED to ON
+        esp32_comms(ser, "GLED_ON")
+    
+    elif RBlink == True:
+        for _ in range(3):
+            esp32_comms(ser, "RLED_ON")
+            sleep(0.2)
+            esp32_comms(ser, "RLED_OFF")
+        
+        #Restore the LED to ON
+        esp32_comms(ser, "RLED_ON")
+    
 
 def start_loop():
     esp32_comms(ser, "LID_OPEN")
@@ -105,26 +159,32 @@ def start_loop():
     esp32_done()
     esp32_comms(ser, "LID_CLOSE")
     esp32_done()
+    
+
 
     while True:
         if binpresence.is_pressed == True:
             print("Bin Absence Warning")
-            esp32_comms(ser, "GLED_OFF")
-            esp32_done()
-            esp32_comms(ser, "RLED_ON")
+            LED_indicators(0, 1)
 
         else:
-
-            esp32_comms(ser, "GLED_ON")
-            esp32_comms(ser, "RLED_OFF")
-
+            
+            LED_indicators(1, 0)
+                        
             print("Checking for humans")
             while True:
                 humanPres(0.3, 5)  # Parameter specifies human detection range in meters,
                 # second parameter specifies timeout value before it autosorts
 
 
+
+
+
+
 def humanPres(detect_range, lid_timeout):
+
+    
+
     ult_distance = float(ult_sensor.distance)
 
     print("Utrasonic Dist: ", ult_distance)
@@ -134,9 +194,8 @@ def humanPres(detect_range, lid_timeout):
 
         # open the lid
         esp32_comms(ser, "LID_OPEN")
-        esp32_done()
         esp32_comms(ser, "EMAG_ON")
-        esp32_done()
+        LED_indicators(1, 0, GBlink = True)
 
         while True:
 
@@ -159,62 +218,82 @@ def humanPres(detect_range, lid_timeout):
 
     elif ult_distance > detect_range:
         pass
-
+        
         # Close the lid
-        # esp32_comms(ser, "LID_CLOSE")
-        # sleep(0.4)
+        #esp32_comms(ser, "LID_CLOSE")
+        #sleep(0.4)
 
 
 def MetalBinTilt():
+
+    plat_servo.angle = 16
+    sleep(0.7)
     plat_servo.angle = 20
     sleep(1)
     plat_servo.angle = 0
-    plat_servo.angle = None
-
-
+    plat_servo.angle= None
+    
 def GenBinTilt():
+    plat_servo.angle = -16
+    sleep(0.7)
     plat_servo.angle = -20
     sleep(1)
     plat_servo.angle = 0
-    plat_servo.angle = None
-
+    plat_servo.angle= None
 
 def SortingCycle():
     # Gate Sequence
     esp32_comms(ser, "LID_CLOSE")
     sleep(0.5)
     esp32_comms(ser, "GATE_OPEN")
-    sleep(5)
+    sleep(4)
     esp32_comms(ser, "GATE_CLOSE")
     sleep(2)
     esp32_comms(ser, "EMAGNET_OFF")
 
-    GS.main()
 
+    #1st Sortation Cycle
+    LED_indicators(1, 0, RBlink = True)
+    GS.main()
     sleep(1)
+    
+    #First sortation cycle ends with GEN waste bring tilted to the other platform
+    GenBinTilt()
+    
+    
 
-    # Platoform servo is connected to Pi Side
-    # First sortation cycle ends with GENERAL waste bring tilted to one platform
-
+    #Platoform servo is connected to Pi Side
+    #First sortation cycle ends with GENERAL waste bring tilted to one platform
+    
+    
+    #2nd Sortation Cycle
     esp32_comms(ser, "GATE_OPEN")
-    sleep(5)
+    sleep(4)
     esp32_comms(ser, "GATE_CLOSE")
-
     GS.main()
-
-    # Platoform servo is connected to Pi Side
-    # First sortation cycle ends with METAL waste bring tilted to the other platform
-
+    
+    #Second
+    MetalBinTilt()
+    
+    
     return
 
 
-# Remove lines below?
-# if __name__ == "__main__":
-# start_loop()
-# return
 
 
-# The main program
+#Remove lines below?
+#if __name__ == "__main__":
+       # start_loop()
+        #return
+
+
+
+
+#The main program
+
+
+
+
 
 
 start_loop()
